36230337ee0bf8c5c3ebc7002fc92a79b81f9544
JhaapKaur <jhaap@hotmail.com>
2023-01-06 21:32:39 +0000

    Component Dictionary added
    To create search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} className="App-logo" alt="App-logo img-fluid" />
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <img src={logo} className="App-logo" alt="App-logo img-fluid" />
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer"> Coded by Jhaap Kaur</footer>
+      </div>
     </div>
   );
 }
