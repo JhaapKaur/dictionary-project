ca313737d3cebb433dab076c5d8b3affd94042dd
JhaapKaur <jhaap@hotmail.com>
2023-01-17 14:50:35 +0000

    authorization added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,7 +26,7 @@ export default function Dictionary(props) {
     let pexelsApiKey =
       "563492ad6f91700001000001914817e43bbc49c7b1738e776025011a";
     let pexelsApiUrl = `"https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
-    let headers = { Autorization: `Bearer ${pexelsApiKey}` };
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
